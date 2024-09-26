e3094c1a03287cc30fc295d0a325e13c7aed01dd
DaryaTrifonova <113592931+DaryaTrifonova@users.noreply.github.com>
2024-09-26 11:07:28 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,8 +2,7 @@ import os
 
 SECRET_KEY = 'secret-key'
 
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\User\\Desktop\\даша\\сессия\\веб\\std_2440_exam.sqlite3'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\User\\Desktop\\даша\\сессия\\веб\\std_2440_exam.sqlite3'
+
 SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2440_exam:Thetreeoflive2005@std-mysql.ist.mospolytech.ru/std_2440_exam'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
